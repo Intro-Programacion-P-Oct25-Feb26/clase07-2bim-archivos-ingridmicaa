3e6c320aefb607e4a1aa3381addc9a46e19f6521
ingridmicaa <ingridmicaela03@gmail.com>
2026-01-29 14:56:38 -0500

    escritura05

--- ManejoArchivos/src/escritura05/Principal.java
+++ ManejoArchivos/src/escritura05/Principal.java
@@ -16,58 +16,48 @@ public class Principal {
 
     public static void main(String args[]) {
         Scanner entrada = new Scanner(System.in);
-    String cadenaFinal = "";
-    String placa = "";
-    boolean bandera = true;
+        String cadenaFinal = "";
+        String placa = "";
+        boolean bandera = true;
 
-    while (bandera) {
+        while (bandera) {
 
-        System.out.println("Ingrese su nombre");
-        String nombre = entrada.nextLine();
-        System.out.println("Ingrese su cedula");
-        String cedula = entrada.nextLine();
-        System.out.println("Ingrese su marca de vehículo");
-        String marca = entrada.nextLine();
-        System.out.println("Ingrese su placa");
-        placa = entrada.nextLine();
-        
-        String placaMayuscula = placa.toUpperCase();
-        String inicialPlaca = placaMayuscula.substring(0, 1);
+            System.out.println("Ingrese su nombre");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese su cedula");
+            String cedula = entrada.nextLine();
+            System.out.println("Ingrese su marca de vehículo");
+            String marca = entrada.nextLine();
+            System.out.println("Ingrese su placa");
+            placa = entrada.nextLine();
 
-      
-        cadenaFinal = String.format("%s | %s | %s | %s\n",
-                nombre, cedula, marca, placaMayuscula);
+            String placaMayuscula = placa.toUpperCase();
+            String inicialPlaca = placaMayuscula.substring(0, 1);
 
-        
-        if (inicialPlaca.equals("L")) {
-            Placas.Loja(cadenaFinal);
-        } else {
-            if (inicialPlaca.equals("P")) {
-                Placas.Pichincha(cadenaFinal);
+            cadenaFinal = String.format("%s | %s | %s | %s\n",
+                    nombre, cedula, marca, placaMayuscula);
+
+            if (inicialPlaca.equals("L")) {
+                Placas.Loja(cadenaFinal);
             } else {
-                if (inicialPlaca.equals("G")) {
-                    Placas.Guayas(cadenaFinal);
+                if (inicialPlaca.equals("P")) {
+                    Placas.Pichincha(cadenaFinal);
                 } else {
-                    Placas.Otros(cadenaFinal);
+                    if (inicialPlaca.equals("G")) {
+                        Placas.Guayas(cadenaFinal);
+                    } else {
+                        Placas.Otros(cadenaFinal);
+                    }
                 }
             }
-        } 
 
-        System.out.println("Si desea continuar ingrese 'si', para salir ingrese 'no'");
-        String opcion = entrada.nextLine(); 
-        
-        if (opcion.equalsIgnoreCase("no")) {
-            bandera = false;
+            System.out.println("Si desea continuar ingrese 'si', para salir ingrese 'no'");
+            String opcion = entrada.nextLine();
+
+            if (opcion.equals("no")) {
+                bandera = false;
+            }
+
         }
-        
-    } 
+    }
 }
-}
-
-    
-
-
-
-
-
-
